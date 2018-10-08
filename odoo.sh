170c8bc01507c940721ea947fa08312405f731eb
Akhter-uz-zaman <arif.cse28@gmail.com>
2018-10-08 10:39:16 +0600

    Added odoo restart method.

--- odoo.sh
+++ odoo.sh
@@ -8,6 +8,33 @@ ODOO_ADDONS_PATH="/opt/odoo/odoo/addons"
 ODOO_CUSTOM_ADDONS_PATH="/opt/odoo/odoo/custom_addons"
 ODOO_STAGING_SERVER_IP="10.10.16.6"
 ODOO_STAGING_SERVER_PATH="$ODOO_SERVER_USERNAME@$ODOO_STAGING_SERVER_IP"
+ODOO_PROJECT_PORT="8069"
+
+restartOdoo(){
+    if [ -z "$1" ]; then
+        echo "Null value is not allowed as first parameter for method! You must pass the required parameter(s)."
+        return $1
+    fi;
+
+    if [ -z "$2" ]; then
+        echo "Null value is not allowed as first parameter for method! You must pass the required parameter(s)."
+        return $2
+    fi;
+
+    if [ -z "$3" ]; then
+        echo "Null value is not allowed as first parameter for method! You must pass the required parameter(s)."
+        return $3
+    fi;
+
+    if [ -z "$4" ]; then
+        echo "Null value is not allowed as first parameter for method! You must pass the required parameter(s)."
+        return $4
+    fi;
+
+    kill $(lsof -t -i:$1)
+    cd $2
+    python3 odoo-bin --addons-path="$3,$4"
+}
 
 runOdooProject() {
     if [ -z "$1" ]; then
@@ -35,3 +62,4 @@ alias ${ODOO_PROJECT_NAME}_addons='cd $ODOO_ADDONS_PATH'
 alias ${ODOO_PROJECT_NAME}_custom_addons='cd $ODOO_CUSTOM_ADDONS_PATH'
 alias ${ODOO_PROJECT_NAME}_run='runOdooProject $ODOO_PROJECT_PATH $ODOO_ADDONS_PATH $ODOO_CUSTOM_ADDONS_PATH'
 alias ${ODOO_PROJECT_NAME}_server='ssh $ODOO_STAGING_SERVER_PATH'
+alias ${ODOO_PROJECT_NAME}_restart='restartOdoo $ODOO_PROJECT_PORT $ODOO_PROJECT_PATH $ODOO_ADDONS_PATH $ODOO_CUSTOM_ADDONS_PATH'
